13c125a67d160090b2abd2b843299538b60a1a55
Chris Yeninas <844685+PhantomGamers@users.noreply.github.com>
2021-09-30 15:29:48 -0400

    remove duplicate line

--- Plugin.cs
+++ Plugin.cs
@@ -29,7 +29,6 @@ namespace AchievementsEnabler
         [HarmonyPatch(typeof(STEAMX), nameof(STEAMX.UnlockAchievement))]
         // Disables unlocking achievements on RailWorks
         [HarmonyPatch(typeof(RAILX), nameof(RAILX.UnlockAchievement))]
-        [HarmonyPatch(typeof(RAILX), nameof(RAILX.UnlockAchievement))]
         // Disables uploading data to Milky Way
         [HarmonyPatch(typeof(PARTNER), nameof(PARTNER.UploadClusterGenerationToGalaxyServer))]
         public static bool Prefix()
